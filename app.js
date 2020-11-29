9d881a56bd5559a8559b5a39a92160afd9860da3
Julio Cefnar Auyon R <juliorecinos30@gmail.com>
2020-11-29 01:02:39 -0600

    Vue.js V.50
    Created:
    Attack Monster method
    Attack Player method
    playerHealth daata property.
    monsterHealth data property

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // Initial Template
+
+function getRandomValue(min, max){
+
+    const attackValue = Math.floor(Math.random() * (max -min)) + min;
+
+
+}
+
+const app = Vue.createApp({
+
+    data (){
+        return {
+            playerHealth: 100,
+            monstearHealth: 100
+
+
+
+        };
+    },
+
+    methods: {
+        attackMonster(){
+            const attackValue = getRandomValue(5, 12);
+            this.monstearHealth -= attackValue;
+            this.attackPlayer;
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+
+        }
+
+
+
+    }
+
+});
+
+app.mount('#game');
