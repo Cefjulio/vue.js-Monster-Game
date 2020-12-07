c65efcd0f6eacbc6b3f528ac7456c35d60e687dd
Julio Cefnar Auyon R <juliorecinos30@gmail.com>
2020-12-06 19:37:10 -0600

    55. Finishing the core functionality

--- app.js
+++ app.js
@@ -22,10 +22,20 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles(){
+            if(this.monsterHealth < 0){
+                return {width: '0%'};
+
+
+            }
             return { width: this.monsterHealth + '%'  }
 
         },
         playerBarStyles(){
+            if(this.playerHealth < 0){
+                return { width: '0%'};
+            }
+
+
             return { width: this.playerHealth + '%'  };
         },
         mayUseSpecialAttack(){
@@ -59,6 +69,15 @@ const app = Vue.createApp({
     },
 
     methods: {
+    
+        startGame ()  {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+        },
+
+
         attackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
@@ -86,6 +105,11 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+
+
+        },
+        surrender(){
+            this.winner = 'monster';
 
 
         }
